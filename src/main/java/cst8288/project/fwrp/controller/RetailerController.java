bd25b710ece10b1a0c8691a6cb7beb4872837134
Loy Yee Ko <koloyyee@gmail.com>
2024-07-17 17:35:18 -0400

    user type checker added to header.jsp

--- src/main/java/cst8288/project/fwrp/controller/RetailerController.java
+++ src/main/java/cst8288/project/fwrp/controller/RetailerController.java
@@ -8,34 +8,47 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Servlet implementation class RetailerController
+ * Servlet implementation class RetailerController<br>
+ *  
+ * <ul>
+ *   <li>[] View all items: if the item is 1 week before expiry, it should show as surplus.</li>
+ *   <li>[] View all surplus</li>
+ *   <li>[] Create, update, and delete items</li>
+ *   <li>[] Change item status as donated, purchased</li>
+ *   <li>[] Set if the item is for “donation”</li>
+ *   <li>[] Set quantities</li>
+ *   <li>[] Set discount rate</li>
+ * </ul>
  */
-@WebServlet(name = "RetailerController", urlPatterns = {"/retailers/*"})
+@WebServlet(name = "RetailerController", urlPatterns = { "/retailers/*" })
 public class RetailerController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RetailerController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String path = request.getPathInfo();
-		
+	public RetailerController() {
+		super();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String path = request.getPathInfo();
+		
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
