7bd6a5226ae4de1376b98bb25a86422c10b71fe0
Loy Yee Ko <koloyyee@gmail.com>
2024-07-25 08:30:23 -0400

    added javadoc to email service

--- src/main/java/cst8288/project/fwrp/service/EmailService.java
+++ src/main/java/cst8288/project/fwrp/service/EmailService.java
@@ -13,36 +13,50 @@ import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
 
+/**
+ * EmailService class
+ * <p>
+ * reference:
+ * https://mkyong.com/java/javamail-api-sending-email-via-gmail-smtp-example/
+ * </p>
+ * <p>
+ * This service is used when Retailer mark item as surplus performs a "pub/sub"
+ * pattern to notify the Consumer
+ * </p>
+ */
 public class EmailService {
 	private Logger log = Logger.getLogger();
 
-//	public static void main(String[] args) {
-//		EmailService emailService = new EmailService();
-//        emailService.send("koloyyee@gmail.com","Hello");
-//	}
-//	
 	public void send(String to, String message) {
 		// send email with JavaMail
 
 		// Email related properties are loaded from application.properties
+		// set email properties from application.properties
 		Properties properties = PropertiesLoader.load();
-//		String sender = properties.getProperty("email.sender");
-//		String password = properties.getProperty("email.password");
-		String sender = System.getenv("email.sender");
-		String password = System.getenv("email.password");
+		String sender = properties.getProperty("email.sender");
+		String password = properties.getProperty("email.password");
 
-		log.info("Sender: " + sender);
-		log.info("Password: " + password);
-		
-//		Session session = Session.getInstance(properties, null);
+		/**
+		 * <p>
+		 * Email related properties are loaded from environment variables
+		 * </p>
+		 * <p>
+		 * To set environment variables in Eclipse, go to Run -> Run Configurations ->
+		 * (Tomcat) Environment -> Add
+		 * </p>
+		 * 
+		 */
+//		String sender = System.getenv("email.sender");
+//		String password = System.getenv("email.password");
 
-		Properties prop = new Properties();
-		prop.put("mail.smtp.host", "smtp.gmail.com");
-		prop.put("mail.smtp.port", "587");
-		prop.put("mail.smtp.auth", "true");
-		prop.put("mail.smtp.starttls.enable", "true");
-		
-		Session session = Session.getInstance(prop, new Authenticator() {
+		// Set email properties programmatically.
+//		Properties prop = new Properties();
+//		prop.put("mail.smtp.host", "smtp.gmail.com");
+//		prop.put("mail.smtp.port", "587");
+//		prop.put("mail.smtp.auth", "true");
+//		prop.put("mail.smtp.starttls.enable", "true");
+
+		Session session = Session.getInstance(properties, new Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(sender, password);
@@ -51,11 +65,10 @@ public class EmailService {
 
 		try {
 			MimeMessage msg = new MimeMessage(session);
-			msg.setFrom(new InternetAddress( sender));
+			msg.setFrom(new InternetAddress(sender));
 			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
 			msg.setSubject("New Discounted Items!");
-			msg.setText("Dear Mail Crawler,"
-                    + "\n\n Please do not spam my email!");
+			msg.setText("Dear Mail Crawler," + "\n\n Please do not spam my email!");
 			Transport.send(msg, sender, password);
 
 			log.info("Sent message successfully");
