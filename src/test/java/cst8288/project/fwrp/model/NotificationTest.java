fd3ddd5dd579ca7c031c0b49a4cbcbd374568363
Tony Nguyen <nguy1057@algonquinlive.com>
2024-08-01 15:44:06 -0400

    Created notification tests

---
+++ src/test/java/cst8288/project/fwrp/model/NotificationTest.java
@@ -0,0 +1,54 @@
+package cst8288.project.fwrp.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+public class NotificationTest {
+
+    @Test
+    public void testNotificationBuilder() {
+        User user = new User();
+        Item item = new Item();
+        LocalDateTime sentTime = LocalDateTime.now();
+        
+        Notification notification = new Notification.Builder()
+            .setUser(user)
+            .setItem(item)
+            .setMessage("Test Message")
+            .setCommMethod(CommMethodType.Email)
+            .setSentTime(sentTime)
+            .build();
+
+        assertNotNull(notification);
+        assertEquals(user, notification.getUser());
+        assertEquals(item, notification.getItem());
+        assertEquals("Test Message", notification.getMessage());
+        assertEquals(CommMethodType.Email, notification.getCommMethod());
+        assertEquals(sentTime, notification.getSentTime());
+    }
+    
+    @Test
+    public void testWrongNotificationMethod() {
+        User user = new User();
+        Item item = new Item();
+        LocalDateTime sentTime = LocalDateTime.now();
+        
+        Notification notification = new Notification.Builder()
+            .setUser(user)
+            .setItem(item)
+            .setMessage("Test Message")
+            .setCommMethod(CommMethodType.Both)
+            .setSentTime(sentTime)
+            .build();
+
+        assertNotNull(notification);
+        assertEquals(user, notification.getUser());
+        assertEquals(item, notification.getItem());
+        assertEquals("Test Message", notification.getMessage());
+        assertNotEquals(CommMethodType.Email, notification.getCommMethod());
+        assertEquals(sentTime, notification.getSentTime());
+    }
+}
